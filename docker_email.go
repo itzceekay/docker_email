fb01087dae4b0f870112096a5fcabdd285605c82
Kirubanand Chandrasekaran <kchandrasekaran@pnet-c02qm2lrg8wn.peoplenetonline.com>
2018-08-06 11:32:26 -0500

    init commit

---
+++ docker_email.go
@@ -0,0 +1,28 @@
+package docker_email
+
+import (
+	"log"
+	"net/mail"
+	"strings"
+
+	"github.com/scorredoira/email"
+)
+
+func mainEmail(subject string, message string, toEmail string) {
+	// Connect to the server, authenticate, set the sender and recipient,
+	// and send the email all in one step.
+
+	m := email.NewMessage(subject, message)
+	m.From = mail.Address{Name: "docker_alert", Address: "docker_test_intern@peoplenetonline.com"}
+	m.To = strings.Split("kirubanand_chandrasekaran@trimble.com", ";")
+	err := email.Send(
+		"aspmx.l.google.com:25",
+		nil,
+		m)
+
+	if err != nil {
+		log.Fatal(err)
+
+	}
+
+}
