bcbaab8204814d1fa19c3dc5e513c553a85f4fbf
Kirubanand Chandrasekaran <kchandrasekaran@pnet-c02qm2lrg8wn.peoplenetonline.com>
2018-08-06 11:58:33 -0500

    routes

--- docker_email.go
+++ docker_email.go
@@ -1,20 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"net/mail"
 	"strings"
 
-	"github.com/scorredoira/email"
+	"github.com/gorilla/mux"
+	"github.com/peoplenet/go-util/config"
 )
 
-func main() {
+var router *mux.Router
+
+func regroute() {
+	port := config.GetInt("8000")
+	router = mux.NewRouter()
+	router.HandleFunc("/{email}", mainEmail).Methods("GET")
+	address := fmt.Sprintf(":%d", port)
+	log.Fatal(http.ListenAndServe(address, router))
+}
+
+func mainEmail(w http.ResponseWriter, r *http.Request) {
 	// Connect to the server, authenticate, set the sender and recipient,
 	// and send the email all in one step.
+	params := mux.Vars(r)
+	email := params["email"]
 
 	m := email.NewMessage("subject", "message")
 	m.From = mail.Address{Name: "docker_alert", Address: "docker_test_intern@peoplenetonline.com"}
-	m.To = strings.Split("kirubanand_chandrasekaran@trimble.com", ";")
+	m.To = strings.Split(email, ";")
 	err := email.Send(
 		"aspmx.l.google.com:25",
 		nil,
@@ -25,4 +40,8 @@ func main() {
 
 	}
 
+}
+
+func main() {
+	go regroute()
 }
