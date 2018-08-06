ecbd129d6340b875c5f8a2dce1e1134728e5449b
Kirubanand Chandrasekaran <kchandrasekaran@pnet-c02qm2lrg8wn.peoplenetonline.com>
2018-08-06 11:38:33 -0500

    init commit

--- docker_email.go
+++ docker_email.go
@@ -8,11 +8,11 @@ import (
 	"github.com/scorredoira/email"
 )
 
-func mainEmail(subject string, message string, toEmail string) {
+func main() {
 	// Connect to the server, authenticate, set the sender and recipient,
 	// and send the email all in one step.
 
-	m := email.NewMessage(subject, message)
+	m := email.NewMessage("subject", "message")
 	m.From = mail.Address{Name: "docker_alert", Address: "docker_test_intern@peoplenetonline.com"}
 	m.To = strings.Split("kirubanand_chandrasekaran@trimble.com", ";")
 	err := email.Send(
